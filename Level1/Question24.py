1294c35e8da6aab5fcb43c60698b9fa48732d77f
MarvinKweyu <mkweyu1@gmail.com>
2018-10-27 12:01:43 +0300

    Added three questions to level 1

---
+++ Level1/Question24.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+# Python has many built-in functions, and if you do not know how to use it,
+# you can read document online or find some books.
+# But Python has a built-in document function for every built-in functions.
+# Please write a program to print some Python built-in functions documents, such as abs(), int(), raw_input()
+# And add document for your own function
+
+print("abs documentation...")
+print(abs.__doc__)
+print("\nint documentation...")
+print(int.__doc__)
+print("\ninput documentation...")
+print(input.__doc__)
