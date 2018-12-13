6d65ca14c5d526c7407d04f82087229fec381375
MarvinKweyu <mkweyu1@gmail.com>
2018-12-13 17:24:58 +0300

    edited question36 to print squares instead of numbers

--- Level1/Question36.py
+++ Level1/Question36.py
@@ -4,7 +4,7 @@
 # square of numbers between 1 and 20 (both included).
 
 def make_list():
-    my_list = [value for value in range(1,21)]
+    my_list = [value*2 for value in range(1,21)]
     print(my_list)
     return
 make_list()
